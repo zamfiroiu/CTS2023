ab4edfd6cdbe8eca98fc0521c653a6fae004f95f
Admin <Admin@mbl-ionutz>
2023-03-05 10:29:27 +0200

    Memento

---
+++ CursCTS2023/19Memento/ro/ase/cts/memento/agentie/clase/MementoPachetTuristic.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.memento.agentie.clase;
+
+public class MementoPachetTuristic {
+	double pretPachet;
+
+	public MementoPachetTuristic(double pretPachet) {
+		super();
+		this.pretPachet = pretPachet;
+	}
+
+	public double getPretPachet() {
+		return pretPachet;
+	}	
+}
---
+++ CursCTS2023/19Memento/ro/ase/cts/memento/agentie/program/Program.java
@@ -0,0 +1,31 @@
+package ro.ase.cts.memento.agentie.program;
+
+import ro.ase.cts.memento.agentie.clase.ManagerPacheteTuritice;
+import ro.ase.cts.memento.agentie.clase.PachetTuristic;
+
+public class Program {
+
+	public static void main(String[] args) {
+		PachetTuristic pachet=new PachetTuristic(20);		
+		ManagerPacheteTuritice managerPachete=new ManagerPacheteTuritice();
+		
+		System.out.println(pachet.toString());
+		managerPachete.adaugaMemento(pachet.salvareMemento());
+		pachet.setPret(50);
+		System.out.println(pachet.toString());
+		pachet.setPret(100);
+		managerPachete.adaugaMemento(pachet.salvareMemento());
+		System.out.println(pachet.toString());
+		pachet.setPret(200);
+		System.out.println(pachet.toString());
+
+		try{
+			pachet.undoToMemento(managerPachete.getMemento(0));
+			System.out.println(pachet.toString());
+		}catch(Exception exception){
+			
+		}
+
+	}
+
+}
